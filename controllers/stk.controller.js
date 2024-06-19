ef4430ed536003c7677057da819900dcca0c0026
Ted@Crestwood <ted@crestwood.co.ke>
2024-06-19 12:41:16 +0300

    starter

--- controllers/stk.controller.js
+++ controllers/stk.controller.js
@@ -2,32 +2,35 @@ const Status = require("../models/Status");
 const handlePay = require("../utils/stkPush");
 
 const paymentPush = (req, res) => {
-    const callbackData = req.body;
-    console.log("callbackdata:", callbackData)
-    // Check the result code
-    const result_code = callbackData.Body.stkCallback.ResultCode;
-    if (result_code !== 0) {
-        // If the result code is not 0, there was an error
-        const error_message = callbackData.Body.stkCallback.ResultDesc;
-        const response_data = { ResultCode: result_code, ResultDesc: error_message };
-        return res.json(response_data);
-    }
+    // const callbackData = req.body;
+    // console.log("callbackdata:", callbackData)
+    // // Check the result code
+    // const result_code = callbackData.Body.stkCallback.ResultCode;
+    // if (result_code !== 0) {
+    //     // If the result code is not 0, there was an error
+    //     const error_message = callbackData.Body.stkCallback.ResultDesc;
+    //     const response_data = { ResultCode: result_code, ResultDesc: error_message };
+    //     return res.json(response_data);
+    // }
 
-    // If the result code is 0, the transaction was completed
-    const body = req.body.Body.stkCallback.CallbackMetadata;
+    // // If the result code is 0, the transaction was completed
+    // const body = req.body.Body.stkCallback.CallbackMetadata;
 
-    // Get amount
-    const amountObj = body.Item.find(obj => obj.Name === 'Amount');
-    const amount = amountObj.Value
+    // // Get amount
+    // const amountObj = body.Item.find(obj => obj.Name === 'Amount');
+    // const amount = amountObj.Value
 
-    // Get Mpesa code
-    const codeObj = body.Item.find(obj => obj.Name === 'MpesaReceiptNumber');
-    const mpesaCode = codeObj.Value
+    // // Get Mpesa code
+    // const codeObj = body.Item.find(obj => obj.Name === 'MpesaReceiptNumber');
+    // const mpesaCode = codeObj.Value
 
-    // Get phone number
-    const phoneNumberObj = body.Item.find(obj => obj.Name === 'PhoneNumber');
-    const phone = phoneNumberObj.Value
-    Status.create({phone:phone, amount:amount, statusCode:result_code})
+    // // Get phone number
+    // const phoneNumberObj = body.Item.find(obj => obj.Name === 'PhoneNumber');
+    // const phone = phoneNumberObj.Value
+    // Status.create({phone:phone, amount:amount, statusCode:result_code})
+    const { phone } = req.body;
+
+    Status.create({ phone: phone })
     return res.json("success");
 }
 const transactionStatus = (req, res) => {
